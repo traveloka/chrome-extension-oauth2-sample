7b769d7f696be3eca0fd8480446cb3f7fbe4f97d
Angga Maulana <angga.maulana@traveloka.com>
2021-03-09 19:57:21 +0700

    Remove unnecessary data source

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,10 +2,6 @@ data "aws_caller_identity" "aws_account" {}
 
 data "aws_region" "current" {}
 
-data "aws_kms_alias" "tvlk_secret_alias" {
-  name = "alias/tvlk/ssm/tvlk-secret"
-}
-
 data "template_file" "apigw" {
   template = "${file("${path.module}/templates/apigw.json")}"
 
